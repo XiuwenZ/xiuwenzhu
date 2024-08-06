9799605e46162cb064aed47862ea51170d36b080
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2024-08-05 23:41:15 -0400

    basic site

---
+++ my-react-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <h1>Hi, my name is Xiuwen!</h1>
+      <h2>I'm currently a senior at Brooklyn College double majoring in computer science and computational math.</h2>   
+
+
+    </div>
+
+  );
+}
+
+export default App;
